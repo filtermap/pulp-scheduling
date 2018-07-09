3bf1c9d1d3d6c7c64b2025cf645b5a1d6d729b96
filtermap <reducefiltermap@gmail.com>
2018-07-09 23:47:50 +0900

    implement group name updating

--- static/src/components/Groups.tsx
+++ static/src/components/Groups.tsx
@@ -1,18 +1,27 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as groups from '../modules/groups'
 
 type Props = {
+  dispatch: Dispatch
   groups: groups.Group[]
 }
 
 function Groups(props: Props) {
+  function handleChange(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(groups.updateGroupName(index, event.target.value))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -23,6 +32,14 @@ function Groups(props: Props) {
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
             <Typography>{group.name}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              label="グループ名"
+              defaultValue={group.name}
+              onChange={handleChange(group.index)}
+              margin="normal"
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/groups.ts
+++ static/src/modules/groups.ts
@@ -1,12 +1,39 @@
+const UPDATE_GROUP_NAME = 'UPDATE_GROUP_NAME'
+
 export type Group = {
   index: number
   name: string
+}
+
+type UpdateGroupName = {
+  type: typeof UPDATE_GROUP_NAME
+  index: number
+  name: string
+}
+
+type Action = UpdateGroupName
+
+export function updateGroupName(index: number, name: string): UpdateGroupName {
+  return {
+    index,
+    name,
+    type: UPDATE_GROUP_NAME,
+  }
 }
 
 export type State = Group[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_GROUP_NAME:
+      return state.map(group => {
+        if (group.index !== action.index) {
+          return group
+        }
+        return { ...group, name: action.name }
+      })
+  }
   return state
 }
