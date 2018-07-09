9d6111f56e87dca7a18679f3ecc6e2ee1a043790
filtermap <reducefiltermap@gmail.com>
2018-07-09 23:49:47 +0900

    implement kinmu name updating

--- static/src/components/Kinmus.tsx
+++ static/src/components/Kinmus.tsx
@@ -1,18 +1,27 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as kinmus from '../modules/kinmus'
 
 type Props = {
+  dispatch: Dispatch
   kinmus: kinmus.Kinmu[]
 }
 
 function Kinmus(props: Props) {
+  function handleChange(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(kinmus.updateKinmuName(index, event.target.value))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -23,6 +32,14 @@ function Kinmus(props: Props) {
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
             <Typography>{kinmu.name}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              label="勤務名"
+              defaultValue={kinmu.name}
+              onChange={handleChange(kinmu.index)}
+              margin="normal"
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/kinmus.ts
+++ static/src/modules/kinmus.ts
@@ -1,12 +1,39 @@
+const UPDATE_KINMU_NAME = 'UPDATE_KINMU_NAME'
+
 export type Kinmu = {
   index: number
   name: string
+}
+
+type UpdateKinmuName = {
+  type: typeof UPDATE_KINMU_NAME
+  index: number
+  name: string
+}
+
+type Action = UpdateKinmuName
+
+export function updateKinmuName(index: number, name: string): UpdateKinmuName {
+  return {
+    index,
+    name,
+    type: UPDATE_KINMU_NAME,
+  }
 }
 
 export type State = Kinmu[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_KINMU_NAME:
+      return state.map(kinmu => {
+        if (kinmu.index !== action.index) {
+          return kinmu
+        }
+        return { ...kinmu, name: action.name }
+      })
+  }
   return state
 }
