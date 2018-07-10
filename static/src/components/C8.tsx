19e5ccd8790f33b702d0a1191dd97dfbeb6866da
filtermap <reducefiltermap@gmail.com>
2018-07-10 21:55:33 +0900

    implement c8 updating

--- static/src/components/C8.tsx
+++ static/src/components/C8.tsx
@@ -1,20 +1,35 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as c8 from '../modules/c8'
 import * as kinmus from '../modules/kinmus'
 
 type Props = {
+  dispatch: Dispatch
   c8: c8.C8[]
   kinmus: kinmus.Kinmu[]
 }
 
 function C8(props: Props) {
+  function handleChangeC8KinmuIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c8.updateC8KinmuIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC8MaxNumberOfDays(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c8.updateC8MaxNumberOfDays(index, parseInt(event.target.value, 10)))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -23,8 +38,28 @@ function C8(props: Props) {
       {props.c8.map(c => (
         <ExpansionPanel key={c.index}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name},${c.max_number_of_days}`}</Typography>
+            <Typography>{`${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の間隔日数を${c.max_number_of_days}日以下にする`}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              select={true}
+              label="勤務"
+              value={c.kinmu_index}
+              onChange={handleChangeC8KinmuIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.kinmus.map(kinmu => (
+                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="間隔日数上限"
+              type="number"
+              defaultValue={c.max_number_of_days}
+              onChange={handleChangeC8MaxNumberOfDays(c.index)}
+              fullWidth={true}
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/c8.ts
+++ static/src/modules/c8.ts
@@ -1,13 +1,62 @@
+const UPDATE_C8_KINMU_INDEX = 'UPDATE_C8_KINMU_INDEX'
+const UPDATE_C8_MAX_NUMBER_OF_DAYS = 'UPDATE_C8_MAX_NUMBER_OF_DAYS'
+
 export type C8 = {
   index: number
   kinmu_index: number
   max_number_of_days: number
 }
 
+type UpdateC8KinmuIndex = {
+  type: typeof UPDATE_C8_KINMU_INDEX
+  index: number
+  kinmu_index: number
+}
+
+type UpdateC8MaxNumberOfDays = {
+  type: typeof UPDATE_C8_MAX_NUMBER_OF_DAYS
+  index: number
+  max_number_of_days: number
+}
+
+type Action = UpdateC8KinmuIndex | UpdateC8MaxNumberOfDays
+
+export function updateC8KinmuIndex(index: number, kinmu_index: number): UpdateC8KinmuIndex {
+  return {
+    index,
+    kinmu_index,
+    type: UPDATE_C8_KINMU_INDEX
+  }
+}
+
+export function updateC8MaxNumberOfDays(index: number, max_number_of_days: number): UpdateC8MaxNumberOfDays {
+  return {
+    index,
+    max_number_of_days,
+    type: UPDATE_C8_MAX_NUMBER_OF_DAYS,
+  }
+}
+
 export type State = C8[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_C8_KINMU_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, kinmu_index: action.kinmu_index }
+      })
+    case UPDATE_C8_MAX_NUMBER_OF_DAYS:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, max_number_of_days: action.max_number_of_days }
+      })
+  }
   return state
 }
