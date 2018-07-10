a972138a2c1a5e8902e8765821620c8dd26bf2e5
filtermap <reducefiltermap@gmail.com>
2018-07-10 21:50:43 +0900

    implement c1 updating

--- static/src/components/C1.tsx
+++ static/src/components/C1.tsx
@@ -1,22 +1,52 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as c1 from '../modules/c1'
 import * as groups from '../modules/groups'
 import * as kinmus from '../modules/kinmus'
 
 type Props = {
+  dispatch: Dispatch
   c1: c1.C1[]
   kinmus: kinmus.Kinmu[]
   groups: groups.Group[]
 }
 
 function C1(props: Props) {
+  function handleChangeC1StartDateName(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c1.updateC1StartDateName(index, event.target.value))
+    }
+  }
+  function handleChangeC1StopDateName(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c1.updateC1StopDateName(index, event.target.value))
+    }
+  }
+  function handleChangeC1KinmuIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c1.updateC1KinmuIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC1GroupIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c1.updateC1GroupIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC1MinNumberOfAssignments(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c1.updateC1MinNumberOfAssignments(index, parseInt(event.target.value, 10)))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -25,8 +55,51 @@ function C1(props: Props) {
       {props.c1.map(c => (
         <ExpansionPanel key={c.index}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${c.start_date_name},${c.stop_date_name},${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name},${props.groups.find(group => group.index === c.group_index)!.name},${c.min_number_of_assignments}`}</Typography>
+            <Typography>{`${c.start_date_name}から${c.stop_date_name}までの${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}に${props.groups.find(group => group.index === c.group_index)!.name}から${c.min_number_of_assignments}人以上の職員を割り当てる`}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              label="開始日"
+              defaultValue={c.start_date_name}
+              onChange={handleChangeC1StartDateName(c.index)}
+              fullWidth={true}
+            />
+            <TextField
+              label="終了日"
+              defaultValue={c.stop_date_name}
+              onChange={handleChangeC1StopDateName(c.index)}
+              fullWidth={true}
+            />
+            <TextField
+              select={true}
+              label="勤務"
+              value={c.kinmu_index}
+              onChange={handleChangeC1KinmuIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.kinmus.map(kinmu => (
+                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select={true}
+              label="グループ"
+              value={c.group_index}
+              onChange={handleChangeC1GroupIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.groups.map(group => (
+                <MenuItem key={group.index} value={group.index}>{group.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="割り当て職員数下限"
+              type="number"
+              defaultValue={c.min_number_of_assignments}
+              onChange={handleChangeC1MinNumberOfAssignments(c.index)}
+              fullWidth={true}
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/c1.ts
+++ static/src/modules/c1.ts
@@ -1,3 +1,9 @@
+const UPDATE_C1_START_DATE_NAME = 'UPDATE_C1_START_DATE_NAME'
+const UPDATE_C1_STOP_DATE_NAME = 'UPDATE_C1_STOP_DATE_NAME'
+const UPDATE_C1_KINMU_INDEX = 'UPDATE_C1_KINMU_INDEX'
+const UPDATE_C1_GROUP_INDEX = 'UPDATE_C1_GROUP_INDEX'
+const UPDATE_C1_MIN_NUMBER_OF_ASSIGNMENTS = 'UPDATE_C1_MIN_NUMBER_OF_ASSIGNMENTS'
+
 export type C1 = {
   index: number
   start_date_name: string
@@ -7,10 +13,119 @@ export type C1 = {
   min_number_of_assignments: number
 }
 
+type UpdateC1StartDateName = {
+  type: typeof UPDATE_C1_START_DATE_NAME
+  index: number
+  start_date_name: string
+}
+
+type UpdateC1StopDateName = {
+  type: typeof UPDATE_C1_STOP_DATE_NAME
+  index: number
+  stop_date_name: string
+}
+
+type UpdateC1KinmuIndex = {
+  type: typeof UPDATE_C1_KINMU_INDEX
+  index: number
+  kinmu_index: number
+}
+
+type UpdateC1GroupIndex = {
+  type: typeof UPDATE_C1_GROUP_INDEX
+  index: number
+  group_index: number
+}
+
+type UpdateC1MinNumberOfAssignments = {
+  type: typeof UPDATE_C1_MIN_NUMBER_OF_ASSIGNMENTS
+  index: number
+  min_number_of_assignments: number
+}
+
+type Action = UpdateC1StartDateName | UpdateC1StopDateName | UpdateC1KinmuIndex | UpdateC1GroupIndex | UpdateC1MinNumberOfAssignments
+
+export function updateC1StartDateName(index: number, start_date_name: string): UpdateC1StartDateName {
+  return {
+    index,
+    start_date_name,
+    type: UPDATE_C1_START_DATE_NAME,
+  }
+}
+
+export function updateC1StopDateName(index: number, stop_date_name: string): UpdateC1StopDateName {
+  return {
+    index,
+    stop_date_name,
+    type: UPDATE_C1_STOP_DATE_NAME,
+  }
+}
+
+export function updateC1KinmuIndex(index: number, kinmu_index: number): UpdateC1KinmuIndex {
+  return {
+    index,
+    kinmu_index,
+    type: UPDATE_C1_KINMU_INDEX,
+  }
+}
+
+export function updateC1GroupIndex(index: number, group_index: number): UpdateC1GroupIndex {
+  return {
+    group_index,
+    index,
+    type: UPDATE_C1_GROUP_INDEX,
+  }
+}
+
+export function updateC1MinNumberOfAssignments(index: number, min_number_of_assignments: number): UpdateC1MinNumberOfAssignments {
+  return {
+    index,
+    min_number_of_assignments,
+    type: UPDATE_C1_MIN_NUMBER_OF_ASSIGNMENTS,
+  }
+}
+
 export type State = C1[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_C1_START_DATE_NAME:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, start_date_name: action.start_date_name }
+      })
+    case UPDATE_C1_STOP_DATE_NAME:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, stop_date_name: action.stop_date_name }
+      })
+    case UPDATE_C1_KINMU_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, kinmu_index: action.kinmu_index }
+      })
+    case UPDATE_C1_GROUP_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, group_index: action.group_index }
+      })
+    case UPDATE_C1_MIN_NUMBER_OF_ASSIGNMENTS:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, min_number_of_assignments: action.min_number_of_assignments }
+      })
+  }
   return state
 }
