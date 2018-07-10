3c249e27b8f8cdba9300e263da8ca2f86b0343a1
filtermap <reducefiltermap@gmail.com>
2018-07-10 21:51:36 +0900

    implement c2 updating

--- static/src/components/C2.tsx
+++ static/src/components/C2.tsx
@@ -1,22 +1,52 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+import MenuItem from '../../node_modules/@material-ui/core/MenuItem'
 import * as all from '../modules/all'
 import * as c2 from '../modules/c2'
 import * as groups from '../modules/groups'
 import * as kinmus from '../modules/kinmus'
 
 type Props = {
+  dispatch: Dispatch
   c2: c2.C2[]
   kinmus: kinmus.Kinmu[]
   groups: groups.Group[]
 }
 
 function C2(props: Props) {
+  function handleChangeC2StartDateName(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c2.updateC2StartDateName(index, event.target.value))
+    }
+  }
+  function handleChangeC2StopDateName(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c2.updateC2StopDateName(index, event.target.value))
+    }
+  }
+  function handleChangeC2KinmuIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c2.updateC2KinmuIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC2GroupIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c2.updateC2GroupIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC2MaxNumberOfAssignments(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c2.updateC2MaxNumberOfAssignments(index, parseInt(event.target.value, 10)))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -25,8 +55,51 @@ function C2(props: Props) {
       {props.c2.map(c => (
         <ExpansionPanel key={c.index}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${c.start_date_name},${c.stop_date_name},${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name},${props.groups.find(group => group.index === c.group_index)!.name},${c.max_number_of_assignments}`}</Typography>
+            <Typography>{`${c.start_date_name}から${c.stop_date_name}までの${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}に${props.groups.find(group => group.index === c.group_index)!.name}から${c.max_number_of_assignments}人以下の職員を割り当てる`}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              label="開始日"
+              defaultValue={c.start_date_name}
+              onChange={handleChangeC2StartDateName(c.index)}
+              fullWidth={true}
+            />
+            <TextField
+              label="終了日"
+              defaultValue={c.stop_date_name}
+              onChange={handleChangeC2StopDateName(c.index)}
+              fullWidth={true}
+            />
+            <TextField
+              select={true}
+              label="勤務"
+              value={c.kinmu_index}
+              onChange={handleChangeC2KinmuIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.kinmus.map(kinmu => (
+                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select={true}
+              label="グループ"
+              value={c.group_index}
+              onChange={handleChangeC2GroupIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.groups.map(group => (
+                <MenuItem key={group.index} value={group.index}>{group.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="割り当て職員数上限"
+              type="number"
+              defaultValue={c.max_number_of_assignments}
+              onChange={handleChangeC2MaxNumberOfAssignments(c.index)}
+              fullWidth={true}
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/c2.ts
+++ static/src/modules/c2.ts
@@ -1,3 +1,9 @@
+const UPDATE_C2_START_DATE_NAME = 'UPDATE_C2_START_DATE_NAME'
+const UPDATE_C2_STOP_DATE_NAME = 'UPDATE_C2_STOP_DATE_NAME'
+const UPDATE_C2_KINMU_INDEX = 'UPDATE_C2_KINMU_INDEX'
+const UPDATE_C2_GROUP_INDEX = 'UPDATE_C2_GROUP_INDEX'
+const UPDATE_C2_MAX_NUMBER_OF_ASSIGNMENTS = 'UPDATE_C2_MAX_NUMBER_OF_ASSIGNMENTS'
+
 export type C2 = {
   index: number
   start_date_name: string
@@ -7,10 +13,119 @@ export type C2 = {
   max_number_of_assignments: number
 }
 
+type UpdateC2StartDateName = {
+  type: typeof UPDATE_C2_START_DATE_NAME
+  index: number
+  start_date_name: string
+}
+
+type UpdateC2StopDateName = {
+  type: typeof UPDATE_C2_STOP_DATE_NAME
+  index: number
+  stop_date_name: string
+}
+
+type UpdateC2KinmuIndex = {
+  type: typeof UPDATE_C2_KINMU_INDEX
+  index: number
+  kinmu_index: number
+}
+
+type UpdateC2GroupIndex = {
+  type: typeof UPDATE_C2_GROUP_INDEX
+  index: number
+  group_index: number
+}
+
+type UpdateC2MaxNumberOfAssignments = {
+  type: typeof UPDATE_C2_MAX_NUMBER_OF_ASSIGNMENTS
+  index: number
+  max_number_of_assignments: number
+}
+
+type Action = UpdateC2StartDateName | UpdateC2StopDateName | UpdateC2KinmuIndex | UpdateC2GroupIndex | UpdateC2MaxNumberOfAssignments
+
+export function updateC2StartDateName(index: number, start_date_name: string): UpdateC2StartDateName {
+  return {
+    index,
+    start_date_name,
+    type: UPDATE_C2_START_DATE_NAME,
+  }
+}
+
+export function updateC2StopDateName(index: number, stop_date_name: string): UpdateC2StopDateName {
+  return {
+    index,
+    stop_date_name,
+    type: UPDATE_C2_STOP_DATE_NAME,
+  }
+}
+
+export function updateC2KinmuIndex(index: number, kinmu_index: number): UpdateC2KinmuIndex {
+  return {
+    index,
+    kinmu_index,
+    type: UPDATE_C2_KINMU_INDEX,
+  }
+}
+
+export function updateC2GroupIndex(index: number, group_index: number): UpdateC2GroupIndex {
+  return {
+    group_index,
+    index,
+    type: UPDATE_C2_GROUP_INDEX,
+  }
+}
+
+export function updateC2MaxNumberOfAssignments(index: number, max_number_of_assignments: number): UpdateC2MaxNumberOfAssignments {
+  return {
+    index,
+    max_number_of_assignments,
+    type: UPDATE_C2_MAX_NUMBER_OF_ASSIGNMENTS,
+  }
+}
+
 export type State = C2[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_C2_START_DATE_NAME:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, start_date_name: action.start_date_name }
+      })
+    case UPDATE_C2_STOP_DATE_NAME:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, stop_date_name: action.stop_date_name }
+      })
+    case UPDATE_C2_KINMU_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, kinmu_index: action.kinmu_index }
+      })
+    case UPDATE_C2_GROUP_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, group_index: action.group_index }
+      })
+    case UPDATE_C2_MAX_NUMBER_OF_ASSIGNMENTS:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, max_number_of_assignments: action.max_number_of_assignments }
+      })
+  }
   return state
 }
