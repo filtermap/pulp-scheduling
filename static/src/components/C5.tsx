d142c6d5650506d60f4b3c64b65e1cb188fd5e86
filtermap <reducefiltermap@gmail.com>
2018-07-10 21:54:07 +0900

    implement c5 updating

--- static/src/components/C5.tsx
+++ static/src/components/C5.tsx
@@ -1,20 +1,35 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as c5 from '../modules/c5'
 import * as kinmus from '../modules/kinmus'
 
 type Props = {
+  dispatch: Dispatch
   c5: c5.C5[]
   kinmus: kinmus.Kinmu[]
 }
 
 function C5(props: Props) {
+  function handleChangeC5KinmuIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c5.updateC5KinmuIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC5MinNumberOfDays(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c5.updateC5MinNumberOfDays(index, parseInt(event.target.value, 10)))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -23,8 +38,28 @@ function C5(props: Props) {
       {props.c5.map(c => (
         <ExpansionPanel key={c.index}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name},${c.min_number_of_days}`}</Typography>
+            <Typography>{`${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の連続日数を${c.min_number_of_days}日以上にする`}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              select={true}
+              label="勤務"
+              value={c.kinmu_index}
+              onChange={handleChangeC5KinmuIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.kinmus.map(kinmu => (
+                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="連続日数下限"
+              type="number"
+              defaultValue={c.min_number_of_days}
+              onChange={handleChangeC5MinNumberOfDays(c.index)}
+              fullWidth={true}
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/c5.ts
+++ static/src/modules/c5.ts
@@ -1,13 +1,62 @@
+const UPDATE_C5_KINMU_INDEX = 'UPDATE_C5_KINMU_INDEX'
+const UPDATE_C5_MIN_NUMBER_OF_DAYS = 'UPDATE_C5_MIN_NUMBER_OF_DAYS'
+
 export type C5 = {
   index: number
   kinmu_index: number
   min_number_of_days: number
 }
 
+type UpdateC5KinmuIndex = {
+  type: typeof UPDATE_C5_KINMU_INDEX
+  index: number
+  kinmu_index: number
+}
+
+type UpdateC5MinNumberOfDays = {
+  type: typeof UPDATE_C5_MIN_NUMBER_OF_DAYS
+  index: number
+  min_number_of_days: number
+}
+
+type Action = UpdateC5KinmuIndex | UpdateC5MinNumberOfDays
+
+export function updateC5KinmuIndex(index: number, kinmu_index: number): UpdateC5KinmuIndex {
+  return {
+    index,
+    kinmu_index,
+    type: UPDATE_C5_KINMU_INDEX,
+  }
+}
+
+export function updateC5MinNumberOfDays(index: number, min_number_of_days: number): UpdateC5MinNumberOfDays {
+  return {
+    index,
+    min_number_of_days,
+    type: UPDATE_C5_MIN_NUMBER_OF_DAYS,
+  }
+}
+
 export type State = C5[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_C5_KINMU_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, kinmu_index: action.kinmu_index }
+      })
+    case UPDATE_C5_MIN_NUMBER_OF_DAYS:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, min_number_of_days: action.min_number_of_days }
+      })
+  }
   return state
 }
