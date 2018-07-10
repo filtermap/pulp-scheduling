9bf45e6221d9ab1513497da231122c5ff2dd971c
filtermap <reducefiltermap@gmail.com>
2018-07-10 21:52:03 +0900

    implement c3 updating

--- static/src/components/C3.tsx
+++ static/src/components/C3.tsx
@@ -1,22 +1,42 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as c3 from '../modules/c3'
 import * as kinmus from '../modules/kinmus'
 import * as members from '../modules/members'
 
 type Props = {
+  dispatch: Dispatch
   c3: c3.C3[]
   members: members.Member[]
   kinmus: kinmus.Kinmu[]
 }
 
 function C3(props: Props) {
+  function handleChangeC3MemberIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c3.updateC3MemberIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC3KinmuIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c3.updateC3KinmuIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC3MinNumberOfAssignments(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c3.updateC3MinNumberOfAssignments(index, parseInt(event.target.value, 10)))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -25,8 +45,39 @@ function C3(props: Props) {
       {props.c3.map(c => (
         <ExpansionPanel key={c.index}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.members.find(member => member.index === c.member_index)!.name},${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name},${c.min_number_of_assignments}`}</Typography>
+            <Typography>{`${props.members.find(member => member.index === c.member_index)!.name}に${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}を${c.min_number_of_assignments}回以上割り当てる`}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              select={true}
+              label="職員"
+              value={c.member_index}
+              onChange={handleChangeC3MemberIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.members.map(member => (
+                <MenuItem key={member.index} value={member.index}>{member.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select={true}
+              label="勤務"
+              value={c.kinmu_index}
+              onChange={handleChangeC3KinmuIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.kinmus.map(kinmu => (
+                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="割り当て数下限"
+              type="number"
+              defaultValue={c.min_number_of_assignments}
+              onChange={handleChangeC3MinNumberOfAssignments(c.index)}
+              fullWidth={true}
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/c3.ts
+++ static/src/modules/c3.ts
@@ -1,3 +1,7 @@
+const UPDATE_C3_MEMBER_INDEX = 'UPDATE_C3_MEMBER_INDEX'
+const UPDATE_C3_KINMU_INDEX = 'UPDATE_C3_KINMU_INDEX'
+const UPDATE_C3_MIN_NUMBER_OF_ASSIGNMENTS = 'UPDATE_C3_MIN_NUMBER_OF_ASSIGNMENTS'
+
 export type C3 = {
   index: number
   member_index: number
@@ -5,10 +9,77 @@ export type C3 = {
   min_number_of_assignments: number
 }
 
+type UpdateC3MemberIndex = {
+  type: typeof UPDATE_C3_MEMBER_INDEX
+  index: number
+  member_index: number
+}
+
+type UpdateC3KinmuIndex = {
+  type: typeof UPDATE_C3_KINMU_INDEX
+  index: number
+  kinmu_index: number
+}
+
+type UpdateC3MinNumberOfAssignments = {
+  type: typeof UPDATE_C3_MIN_NUMBER_OF_ASSIGNMENTS
+  index: number
+  min_number_of_assignments: number
+}
+
+type Action = UpdateC3MemberIndex | UpdateC3KinmuIndex | UpdateC3MinNumberOfAssignments
+
+export function updateC3MemberIndex(index: number, member_index: number): UpdateC3MemberIndex {
+  return {
+    index,
+    member_index,
+    type: UPDATE_C3_MEMBER_INDEX,
+  }
+}
+
+export function updateC3KinmuIndex(index: number, kinmu_index: number): UpdateC3KinmuIndex {
+  return {
+    index,
+    kinmu_index,
+    type: UPDATE_C3_KINMU_INDEX,
+  }
+}
+
+export function updateC3MinNumberOfAssignments(index: number, min_number_of_assignments: number): UpdateC3MinNumberOfAssignments {
+  return {
+    index,
+    min_number_of_assignments,
+    type: UPDATE_C3_MIN_NUMBER_OF_ASSIGNMENTS
+  }
+}
+
 export type State = C3[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_C3_MEMBER_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, member_index: action.member_index }
+      })
+    case UPDATE_C3_KINMU_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, kinmu_index: action.kinmu_index }
+      })
+    case UPDATE_C3_MIN_NUMBER_OF_ASSIGNMENTS:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, min_number_of_assignments: action.min_number_of_assignments }
+      })
+  }
   return state
 }
