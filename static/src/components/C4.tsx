d871e137f1ee582b9bff6958ca465d1f9bb2ee68
filtermap <reducefiltermap@gmail.com>
2018-07-10 21:53:42 +0900

    implement c4 updating

--- static/src/components/C4.tsx
+++ static/src/components/C4.tsx
@@ -1,22 +1,42 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as c4 from '../modules/c4'
 import * as kinmus from '../modules/kinmus'
 import * as members from '../modules/members'
 
 type Props = {
+  dispatch: Dispatch
   c4: c4.C4[]
   members: members.Member[]
   kinmus: kinmus.Kinmu[]
 }
 
 function C4(props: Props) {
+  function handleChangeC4MemberIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c4.updateC4MemberIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC4KinmuIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c4.updateC4KinmuIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC4MaxNumberOfAssignments(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c4.updateC4MaxNumberOfAssignments(index, parseInt(event.target.value, 10)))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -25,8 +45,39 @@ function C4(props: Props) {
       {props.c4.map(c => (
         <ExpansionPanel key={c.index}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.members.find(member => member.index === c.member_index)!.name},${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name},${c.max_number_of_assignments}`}</Typography>
+            <Typography>{`${props.members.find(member => member.index === c.member_index)!.name}に${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}を${c.max_number_of_assignments}回以下割り当てる`}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              select={true}
+              label="職員"
+              value={c.member_index}
+              onChange={handleChangeC4MemberIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.members.map(member => (
+                <MenuItem key={member.index} value={member.index}>{member.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select={true}
+              label="勤務"
+              value={c.kinmu_index}
+              onChange={handleChangeC4KinmuIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.kinmus.map(kinmu => (
+                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="割り当て数上限"
+              type="number"
+              defaultValue={c.max_number_of_assignments}
+              onChange={handleChangeC4MaxNumberOfAssignments(c.index)}
+              fullWidth={true}
+            />
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/c4.ts
+++ static/src/modules/c4.ts
@@ -1,3 +1,7 @@
+const UPDATE_C4_MEMBER_INDEX = 'UPDATE_C4_MEMBER_INDEX'
+const UPDATE_C4_KINMU_INDEX = 'UPDATE_C4_KINMU_INDEX'
+const UPDATE_C4_MAX_NUMBER_OF_ASSIGNMENTS = 'UPDATE_C4_MAX_NUMBER_OF_ASSIGNMENTS'
+
 export type C4 = {
   index: number
   member_index: number
@@ -5,10 +9,77 @@ export type C4 = {
   max_number_of_assignments: number
 }
 
+type UpdateC4MemberIndex = {
+  type: typeof UPDATE_C4_MEMBER_INDEX
+  index: number
+  member_index: number
+}
+
+type UpdateC4KinmuIndex = {
+  type: typeof UPDATE_C4_KINMU_INDEX
+  index: number
+  kinmu_index: number
+}
+
+type UpdateC4MaxNumberOfAssignments = {
+  type: typeof UPDATE_C4_MAX_NUMBER_OF_ASSIGNMENTS
+  index: number
+  max_number_of_assignments: number
+}
+
+type Action = UpdateC4MemberIndex | UpdateC4KinmuIndex | UpdateC4MaxNumberOfAssignments
+
+export function updateC4MemberIndex(index: number, member_index: number): UpdateC4MemberIndex {
+  return {
+    index,
+    member_index,
+    type: UPDATE_C4_MEMBER_INDEX,
+  }
+}
+
+export function updateC4KinmuIndex(index: number, kinmu_index: number): UpdateC4KinmuIndex {
+  return {
+    index,
+    kinmu_index,
+    type: UPDATE_C4_KINMU_INDEX,
+  }
+}
+
+export function updateC4MaxNumberOfAssignments(index: number, max_number_of_assignments: number): UpdateC4MaxNumberOfAssignments {
+  return {
+    index,
+    max_number_of_assignments,
+    type: UPDATE_C4_MAX_NUMBER_OF_ASSIGNMENTS
+  }
+}
+
 export type State = C4[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_C4_MEMBER_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, member_index: action.member_index }
+      })
+    case UPDATE_C4_KINMU_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, kinmu_index: action.kinmu_index }
+      })
+    case UPDATE_C4_MAX_NUMBER_OF_ASSIGNMENTS:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, max_number_of_assignments: action.max_number_of_assignments }
+      })
+  }
   return state
 }
