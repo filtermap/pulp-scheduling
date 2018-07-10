493b40ae3e381af6d8d2d13b17889abbe3a9b39b
filtermap <reducefiltermap@gmail.com>
2018-07-10 21:56:12 +0900

    implement c10 updating

--- static/src/components/C10.tsx
+++ static/src/components/C10.tsx
@@ -1,22 +1,47 @@
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as c10 from '../modules/c10'
 import * as kinmus from '../modules/kinmus'
 import * as members from '../modules/members'
 
 type Props = {
+  dispatch: Dispatch
   c10: c10.C10[]
   members: members.Member[]
   kinmus: kinmus.Kinmu[]
 }
 
 function C10(props: Props) {
+  function handleChangeC10MemberIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c10.updateC10MemberIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
+  function handleChangeC10StartDateName(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c10.updateC10StartDateName(index, event.target.value))
+    }
+  }
+  function handleChangeC10StopDateName(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c10.updateC10StopDateName(index, event.target.value))
+    }
+  }
+  function handleChangeC10KinmuIndex(index: number) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.dispatch(c10.updateC10KinmuIndex(index, parseInt(event.target.value, 10)))
+    }
+  }
   return (
     <>
       <Toolbar>
@@ -25,8 +50,44 @@ function C10(props: Props) {
       {props.c10.map(c => (
         <ExpansionPanel key={c.index}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.members.find(member => member.index === c.member_index)!.name},${c.start_date_name},${c.stop_date_name},${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}`}</Typography>
+            <Typography>{`${props.members.find(member => member.index === c.member_index)!.name}の${c.start_date_name}から${c.stop_date_name}までに${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}を割り当てない`}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <TextField
+              select={true}
+              label="職員"
+              value={c.member_index}
+              onChange={handleChangeC10MemberIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.members.map(member => (
+                <MenuItem key={member.index} value={member.index}>{member.name}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="開始日"
+              defaultValue={c.start_date_name}
+              onChange={handleChangeC10StartDateName(c.index)}
+              fullWidth={true}
+            />
+            <TextField
+              label="終了日"
+              defaultValue={c.stop_date_name}
+              onChange={handleChangeC10StopDateName(c.index)}
+              fullWidth={true}
+            />
+            <TextField
+              select={true}
+              label="勤務"
+              value={c.kinmu_index}
+              onChange={handleChangeC10KinmuIndex(c.index)}
+              fullWidth={true}
+            >
+              {props.kinmus.map(kinmu => (
+                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+              ))}
+            </TextField>
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/c10.ts
+++ static/src/modules/c10.ts
@@ -1,3 +1,8 @@
+const UPDATE_C10_MEMBER_INDEX = 'UPDATE_C10_MEMBER_INDEX'
+const UPDATE_C10_START_DATE_NAME = 'UPDATE_C10_START_DATE_NAME'
+const UPDATE_C10_STOP_DATE_NAME = 'UPDATE_C10_STOP_DATE_NAME'
+const UPDATE_C10_KINMU_INDEX = 'UPDATE_C10_KINMU_INDEX'
+
 export type C10 = {
   index: number
   member_index: number
@@ -6,10 +11,98 @@ export type C10 = {
   kinmu_index: number
 }
 
+type UpdateC10MemberIndex = {
+  type: typeof UPDATE_C10_MEMBER_INDEX
+  index: number
+  member_index: number
+}
+
+type UpdateC10StartDateName = {
+  type: typeof UPDATE_C10_START_DATE_NAME
+  index: number
+  start_date_name: string
+}
+
+type UpdateC10StopDateName = {
+  type: typeof UPDATE_C10_STOP_DATE_NAME
+  index: number
+  stop_date_name: string
+}
+
+type UpdateC10KinmuIndex = {
+  type: typeof UPDATE_C10_KINMU_INDEX
+  index: number
+  kinmu_index: number
+}
+
+type Action = UpdateC10MemberIndex | UpdateC10StartDateName | UpdateC10StopDateName | UpdateC10KinmuIndex
+
+export function updateC10MemberIndex(index: number, member_index: number): UpdateC10MemberIndex {
+  return {
+    index,
+    member_index,
+    type: UPDATE_C10_MEMBER_INDEX,
+  }
+}
+
+export function updateC10StartDateName(index: number, start_date_name: string): UpdateC10StartDateName {
+  return {
+    index,
+    start_date_name,
+    type: UPDATE_C10_START_DATE_NAME,
+  }
+}
+
+export function updateC10StopDateName(index: number, stop_date_name: string): UpdateC10StopDateName {
+  return {
+    index,
+    stop_date_name,
+    type: UPDATE_C10_STOP_DATE_NAME,
+  }
+}
+
+export function updateC10KinmuIndex(index: number, kinmu_index: number): UpdateC10KinmuIndex {
+  return {
+    index,
+    kinmu_index,
+    type: UPDATE_C10_KINMU_INDEX,
+  }
+}
+
 export type State = C10[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case UPDATE_C10_MEMBER_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, member_index: action.member_index }
+      })
+    case UPDATE_C10_START_DATE_NAME:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, start_date_name: action.start_date_name }
+      })
+    case UPDATE_C10_STOP_DATE_NAME:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, stop_date_name: action.stop_date_name }
+      })
+    case UPDATE_C10_KINMU_INDEX:
+      return state.map(c => {
+        if (c.index !== action.index) {
+          return c
+        }
+        return { ...c, kinmu_index: action.kinmu_index }
+      })
+  }
   return state
 }
