4be2da746a38988b81cb09a46dad2c880907ccf4
filtermap <reducefiltermap@gmail.com>
2018-07-15 16:53:59 +0900

    implement c5 creation

--- static/src/components/C5.tsx
+++ static/src/components/C5.tsx
@@ -1,4 +1,9 @@
 import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
@@ -15,65 +20,131 @@ import * as all from '../modules/all'
 import * as c5 from '../modules/c5'
 import * as kinmus from '../modules/kinmus'
 
+type State = {
+  open: boolean
+  kinmu_index: number
+  min_number_of_days: number
+}
+
 type Props = {
   dispatch: Dispatch
   c5: c5.C5[]
   kinmus: kinmus.Kinmu[]
 }
 
-function C5(props: Props) {
-  function handleChangeC5KinmuIndex(index: number) {
+class C5 extends React.Component<Props, State> {
+  public state: State = {
+    kinmu_index: 0,
+    min_number_of_days: 0,
+    open: false,
+  }
+  public handleChangeC5KinmuIndex(index: number) {
     return (event: React.ChangeEvent<HTMLInputElement>) => {
-      props.dispatch(c5.updateC5KinmuIndex(index, parseInt(event.target.value, 10)))
+      this.props.dispatch(c5.updateC5KinmuIndex(index, parseInt(event.target.value, 10)))
     }
   }
-  function handleChangeC5MinNumberOfDays(index: number) {
+  public handleChangeC5MinNumberOfDays(index: number) {
     return (event: React.ChangeEvent<HTMLInputElement>) => {
-      props.dispatch(c5.updateC5MinNumberOfDays(index, parseInt(event.target.value, 10)))
+      this.props.dispatch(c5.updateC5MinNumberOfDays(index, parseInt(event.target.value, 10)))
     }
   }
-  function handleClickDeleteC5(index: number) {
+  public handleClickDeleteC5(index: number) {
     return (_: React.MouseEvent<HTMLButtonElement>) => {
-      props.dispatch(c5.deleteC5(index))
+      this.props.dispatch(c5.deleteC5(index))
     }
   }
-  return (
-    <>
-      <Toolbar>
-        <Typography variant="subheading">勤務の連続日数の下限</Typography>
-      </Toolbar>
-      {props.c5.map(c => (
-        <ExpansionPanel key={c.index}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の連続日数を${c.min_number_of_days}日以上にする`}</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <TextField
-              select={true}
-              label="勤務"
-              value={c.kinmu_index}
-              onChange={handleChangeC5KinmuIndex(c.index)}
-              fullWidth={true}
-            >
-              {props.kinmus.map(kinmu => (
-                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              label="連続日数下限"
-              type="number"
-              defaultValue={c.min_number_of_days}
-              onChange={handleChangeC5MinNumberOfDays(c.index)}
-              fullWidth={true}
-            />
-          </ExpansionPanelDetails>
-          <ExpansionPanelActions>
-            <Button size="small" onClick={handleClickDeleteC5(c.index)}>削除</Button>
-          </ExpansionPanelActions>
-        </ExpansionPanel>
-      ))}
-    </>
-  )
+  public handleClickOpenDialog = () => {
+    this.setState({ open: true })
+  }
+  public handleCloseDialog = () => {
+    this.setState({ open: false })
+  }
+  public handleChangeNewC5KinmuIndex = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ kinmu_index: parseInt(event.target.value, 10) })
+  }
+  public handleChangeNewC5MinNumberOfDays = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ min_number_of_days: parseInt(event.target.value, 10) })
+  }
+  public handleClickCreateC5 = () => {
+    this.setState({ open: false })
+    this.props.dispatch(c5.createC5(this.state.kinmu_index, this.state.min_number_of_days))
+  }
+  public render() {
+    return (
+      <>
+        <Toolbar>
+          <Typography variant="subheading" style={{ flex: 1 }}>勤務の連続日数の下限</Typography>
+          <Button size="small" onClick={this.handleClickOpenDialog}>追加</Button>
+        </Toolbar>
+        {this.props.c5.map(c => (
+          <ExpansionPanel key={c.index}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>{`${this.props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の連続日数を${c.min_number_of_days}日以上にする`}</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <TextField
+                select={true}
+                label="勤務"
+                value={c.kinmu_index}
+                onChange={this.handleChangeC5KinmuIndex(c.index)}
+                fullWidth={true}
+              >
+                {this.props.kinmus.map(kinmu => (
+                  <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                label="連続日数下限"
+                type="number"
+                defaultValue={c.min_number_of_days}
+                onChange={this.handleChangeC5MinNumberOfDays(c.index)}
+                fullWidth={true}
+              />
+            </ExpansionPanelDetails>
+            <ExpansionPanelActions>
+              <Button size="small" onClick={this.handleClickDeleteC5(c.index)}>削除</Button>
+            </ExpansionPanelActions>
+          </ExpansionPanel>
+        ))}
+        {this.props.kinmus.length === 0 ?
+          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+            <DialogTitle>勤務の連続日数の下限を追加できません</DialogTitle>
+            <DialogContent>
+              {this.props.kinmus.length === 0 ? <DialogContentText>勤務がありません</DialogContentText> : null}
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={this.handleCloseDialog}>閉じる</Button>
+            </DialogActions>
+          </Dialog> :
+          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+            <DialogTitle>勤務の連続日数の下限の追加</DialogTitle>
+            <DialogContent style={{ display: 'flex' }}>
+              <TextField
+                select={true}
+                label="勤務"
+                value={this.state.kinmu_index}
+                onChange={this.handleChangeNewC5KinmuIndex}
+                fullWidth={true}
+              >
+                {this.props.kinmus.map(kinmu => (
+                  <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                label="連続日数下限"
+                type="number"
+                defaultValue={this.state.min_number_of_days}
+                onChange={this.handleChangeNewC5MinNumberOfDays}
+                fullWidth={true}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={this.handleClickCreateC5}>追加</Button>
+            </DialogActions>
+          </Dialog>}
+      </>
+    )
+  }
 }
 
 function mapStateToProps(state: all.State) {
--- static/src/modules/c5.ts
+++ static/src/modules/c5.ts
@@ -1,9 +1,16 @@
+const CREATE_C5 = 'CREATE_C5'
 const UPDATE_C5_KINMU_INDEX = 'UPDATE_C5_KINMU_INDEX'
 const UPDATE_C5_MIN_NUMBER_OF_DAYS = 'UPDATE_C5_MIN_NUMBER_OF_DAYS'
 const DELETE_C5 = 'DELETE_C5'
 
 export type C5 = {
   index: number
+  kinmu_index: number
+  min_number_of_days: number
+}
+
+type CreateC5 = {
+  type: typeof CREATE_C5
   kinmu_index: number
   min_number_of_days: number
 }
@@ -26,9 +33,18 @@ type DeleteC5 = {
 }
 
 type Action =
+  | CreateC5
   | UpdateC5KinmuIndex
   | UpdateC5MinNumberOfDays
   | DeleteC5
+
+export function createC5(kinmu_index: number, min_number_of_days: number): CreateC5 {
+  return {
+    kinmu_index,
+    min_number_of_days,
+    type: CREATE_C5,
+  }
+}
 
 export function updateC5KinmuIndex(index: number, kinmu_index: number): UpdateC5KinmuIndex {
   return {
@@ -59,6 +75,12 @@ const initialState: State = []
 
 export function reducer(state: State = initialState, action: Action): State {
   switch (action.type) {
+    case CREATE_C5:
+      return state.concat({
+        index: Math.max(...state.map(c => c.index)) + 1,
+        kinmu_index: action.kinmu_index,
+        min_number_of_days: action.min_number_of_days,
+      })
     case UPDATE_C5_KINMU_INDEX:
       return state.map(c => {
         if (c.index !== action.index) {
