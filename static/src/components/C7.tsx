388a62867437522277163401add6c6cff28bca3a
filtermap <reducefiltermap@gmail.com>
2018-07-15 17:45:09 +0900

    implement c7 creation

--- static/src/components/C7.tsx
+++ static/src/components/C7.tsx
@@ -1,4 +1,9 @@
 import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails/ExpansionPanelDetails'
@@ -15,65 +20,131 @@ import * as all from '../modules/all'
 import * as c7 from '../modules/c7'
 import * as kinmus from '../modules/kinmus'
 
+type State = {
+  open: boolean
+  kinmu_index: number
+  min_number_of_days: number
+}
+
 type Props = {
   dispatch: Dispatch
   c7: c7.C7[]
   kinmus: kinmus.Kinmu[]
 }
 
-function C7(props: Props) {
-  function handleChangeC7KinmuIndex(index: number) {
+class C7 extends React.Component<Props, State> {
+  public state: State = {
+    kinmu_index: 0,
+    min_number_of_days: 0,
+    open: false,
+  }
+  public handleChangeC7KinmuIndex(index: number) {
     return (event: React.ChangeEvent<HTMLInputElement>) => {
-      props.dispatch(c7.updateC7KinmuIndex(index, parseInt(event.target.value, 10)))
+      this.props.dispatch(c7.updateC7KinmuIndex(index, parseInt(event.target.value, 10)))
     }
   }
-  function handleChangeC7MinNumberOfDays(index: number) {
+  public handleChangeC7MinNumberOfDays(index: number) {
     return (event: React.ChangeEvent<HTMLInputElement>) => {
-      props.dispatch(c7.updateC7MinNumberOfDays(index, parseInt(event.target.value, 10)))
+      this.props.dispatch(c7.updateC7MinNumberOfDays(index, parseInt(event.target.value, 10)))
     }
   }
-  function handleClickDeleteC7(index: number) {
+  public handleClickDeleteC7(index: number) {
     return (_: React.MouseEvent<HTMLButtonElement>) => {
-      props.dispatch(c7.deleteC7(index))
+      this.props.dispatch(c7.deleteC7(index))
     }
   }
-  return (
-    <>
-      <Toolbar>
-        <Typography variant="subheading">勤務の間隔日数の下限</Typography>
-      </Toolbar>
-      {props.c7.map(c => (
-        <ExpansionPanel key={c.index}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の間隔日数を${c.min_number_of_days}日以上にする`}</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <TextField
-              select={true}
-              label="勤務"
-              value={c.kinmu_index}
-              onChange={handleChangeC7KinmuIndex(c.index)}
-              fullWidth={true}
-            >
-              {props.kinmus.map(kinmu => (
-                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              label="間隔日数下限"
-              type="number"
-              defaultValue={c.min_number_of_days}
-              onChange={handleChangeC7MinNumberOfDays(c.index)}
-              fullWidth={true}
-            />
-          </ExpansionPanelDetails>
-          <ExpansionPanelActions>
-            <Button size="small" onClick={handleClickDeleteC7(c.index)}>削除</Button>
-          </ExpansionPanelActions>
-        </ExpansionPanel>
-      ))}
-    </>
-  )
+  public handleClickOpenDialog = () => {
+    this.setState({ open: true })
+  }
+  public handleCloseDialog = () => {
+    this.setState({ open: false })
+  }
+  public handleChangeNewC7KinmuIndex = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ kinmu_index: parseInt(event.target.value, 10) })
+  }
+  public handleChangeNewC7MinNumberOfDays = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ min_number_of_days: parseInt(event.target.value, 10) })
+  }
+  public handleClickCreateC7 = () => {
+    this.setState({ open: false })
+    this.props.dispatch(c7.createC7(this.state.kinmu_index, this.state.min_number_of_days))
+  }
+  public render() {
+    return (
+      <>
+        <Toolbar>
+          <Typography variant="subheading" style={{ flex: 1 }}>勤務の間隔日数の下限</Typography>
+          <Button size="small" onClick={this.handleClickOpenDialog}>追加</Button>
+        </Toolbar>
+        {this.props.c7.map(c => (
+          <ExpansionPanel key={c.index}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>{`${this.props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の間隔日数を${c.min_number_of_days}日以上にする`}</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <TextField
+                select={true}
+                label="勤務"
+                value={c.kinmu_index}
+                onChange={this.handleChangeC7KinmuIndex(c.index)}
+                fullWidth={true}
+              >
+                {this.props.kinmus.map(kinmu => (
+                  <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                label="間隔日数下限"
+                type="number"
+                defaultValue={c.min_number_of_days}
+                onChange={this.handleChangeC7MinNumberOfDays(c.index)}
+                fullWidth={true}
+              />
+            </ExpansionPanelDetails>
+            <ExpansionPanelActions>
+              <Button size="small" onClick={this.handleClickDeleteC7(c.index)}>削除</Button>
+            </ExpansionPanelActions>
+          </ExpansionPanel>
+        ))}
+        {this.props.kinmus.length === 0 ?
+          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+            <DialogTitle>勤務の間隔日数の下限を追加できません</DialogTitle>
+            <DialogContent>
+              {this.props.kinmus.length === 0 ? <DialogContentText>勤務がありません</DialogContentText> : null}
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={this.handleCloseDialog}>閉じる</Button>
+            </DialogActions>
+          </Dialog> :
+          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+            <DialogTitle>勤務の間隔日数の下限の追加</DialogTitle>
+            <DialogContent style={{ display: 'flex' }}>
+              <TextField
+                select={true}
+                label="勤務"
+                value={this.state.kinmu_index}
+                onChange={this.handleChangeNewC7KinmuIndex}
+                fullWidth={true}
+              >
+                {this.props.kinmus.map(kinmu => (
+                  <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                label="間隔日数下限"
+                type="number"
+                defaultValue={this.state.min_number_of_days}
+                onChange={this.handleChangeNewC7MinNumberOfDays}
+                fullWidth={true}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={this.handleClickCreateC7}>追加</Button>
+            </DialogActions>
+          </Dialog>}
+      </>
+    )
+  }
 }
 
 function mapStateToProps(state: all.State) {
--- static/src/modules/c7.ts
+++ static/src/modules/c7.ts
@@ -1,9 +1,16 @@
+const CREATE_C7 = 'CREATE_C7'
 const UPDATE_C7_KINMU_INDEX = 'UPDATE_C7_KINMU_INDEX'
 const UPDATE_C7_MIN_NUMBER_OF_DAYS = 'UPDATE_C7_MIN_NUMBER_OF_DAYS'
 const DELETE_C7 = 'DELETE_C7'
 
 export type C7 = {
   index: number
+  kinmu_index: number
+  min_number_of_days: number
+}
+
+type CreateC7 = {
+  type: typeof CREATE_C7
   kinmu_index: number
   min_number_of_days: number
 }
@@ -26,9 +33,18 @@ type DeleteC7 = {
 }
 
 type Action =
+  | CreateC7
   | UpdateC7KinmuIndex
   | UpdateC7MinNumberOfDays
   | DeleteC7
+
+export function createC7(kinmu_index: number, min_number_of_days: number): CreateC7 {
+  return {
+    kinmu_index,
+    min_number_of_days,
+    type: CREATE_C7,
+  }
+}
 
 export function updateC7KinmuIndex(index: number, kinmu_index: number): UpdateC7KinmuIndex {
   return {
@@ -59,6 +75,12 @@ const initialState: State = []
 
 export function reducer(state: State = initialState, action: Action): State {
   switch (action.type) {
+    case CREATE_C7:
+      return state.concat({
+        index: Math.max(...state.map(c => c.index)) + 1,
+        kinmu_index: action.kinmu_index,
+        min_number_of_days: action.min_number_of_days,
+      })
     case UPDATE_C7_KINMU_INDEX:
       return state.map(c => {
         if (c.index !== action.index) {
