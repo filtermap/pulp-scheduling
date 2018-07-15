c06c54eaead53e3a21c1da984ef954090c99c045
filtermap <reducefiltermap@gmail.com>
2018-07-15 17:05:54 +0900

    implement c6 creation

--- static/src/components/C6.tsx
+++ static/src/components/C6.tsx
@@ -1,4 +1,9 @@
 import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
@@ -15,65 +20,131 @@ import * as all from '../modules/all'
 import * as c6 from '../modules/c6'
 import * as kinmus from '../modules/kinmus'
 
+type State = {
+  open: boolean
+  kinmu_index: number
+  max_number_of_days: number
+}
+
 type Props = {
   dispatch: Dispatch
   c6: c6.C6[]
   kinmus: kinmus.Kinmu[]
 }
 
-function C6(props: Props) {
-  function handleChangeC6KinmuIndex(index: number) {
+class C6 extends React.Component<Props, State> {
+  public state: State = {
+    kinmu_index: 0,
+    max_number_of_days: 0,
+    open: false,
+  }
+  public handleChangeC6KinmuIndex(index: number) {
     return (event: React.ChangeEvent<HTMLInputElement>) => {
-      props.dispatch(c6.updateC6KinmuIndex(index, parseInt(event.target.value, 10)))
+      this.props.dispatch(c6.updateC6KinmuIndex(index, parseInt(event.target.value, 10)))
     }
   }
-  function handleChangeC6MaxNumberOfDays(index: number) {
+  public handleChangeC6MaxNumberOfDays(index: number) {
     return (event: React.ChangeEvent<HTMLInputElement>) => {
-      props.dispatch(c6.updateC6MaxNumberOfDays(index, parseInt(event.target.value, 10)))
+      this.props.dispatch(c6.updateC6MaxNumberOfDays(index, parseInt(event.target.value, 10)))
     }
   }
-  function handleClickDeleteC6(index: number) {
+  public handleClickDeleteC6(index: number) {
     return (_: React.MouseEvent<HTMLButtonElement>) => {
-      props.dispatch(c6.deleteC6(index))
+      this.props.dispatch(c6.deleteC6(index))
     }
   }
-  return (
-    <>
-      <Toolbar>
-        <Typography variant="subheading">勤務の連続日数の上限</Typography>
-      </Toolbar>
-      {props.c6.map(c => (
-        <ExpansionPanel key={c.index}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の連続日数を${c.max_number_of_days}日以下にする`}</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <TextField
-              select={true}
-              label="勤務"
-              value={c.kinmu_index}
-              onChange={handleChangeC6KinmuIndex(c.index)}
-              fullWidth={true}
-            >
-              {props.kinmus.map(kinmu => (
-                <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              label="連続日数上限"
-              type="number"
-              defaultValue={c.max_number_of_days}
-              onChange={handleChangeC6MaxNumberOfDays(c.index)}
-              fullWidth={true}
-            />
-          </ExpansionPanelDetails>
-          <ExpansionPanelActions>
-            <Button size="small" onClick={handleClickDeleteC6(c.index)}>削除</Button>
-          </ExpansionPanelActions>
-        </ExpansionPanel>
-      ))}
-    </>
-  )
+  public handleClickOpenDialog = () => {
+    this.setState({ open: true })
+  }
+  public handleCloseDialog = () => {
+    this.setState({ open: false })
+  }
+  public handleChangeNewC6KinmuIndex = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ kinmu_index: parseInt(event.target.value, 10) })
+  }
+  public handleChangeNewC6MaxNumberOfDays = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ max_number_of_days: parseInt(event.target.value, 10) })
+  }
+  public handleClickCreateC6 = () => {
+    this.setState({ open: false })
+    this.props.dispatch(c6.createC6(this.state.kinmu_index, this.state.max_number_of_days))
+  }
+  public render() {
+    return (
+      <>
+        <Toolbar>
+          <Typography variant="subheading" style={{ flex: 1 }}>勤務の連続日数の上限</Typography>
+          <Button size="small" onClick={this.handleClickOpenDialog}>追加</Button>
+        </Toolbar>
+        {this.props.c6.map(c => (
+          <ExpansionPanel key={c.index}>
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>{`${this.props.kinmus.find(kinmu => kinmu.index === c.kinmu_index)!.name}の連続日数を${c.max_number_of_days}日以下にする`}</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <TextField
+                select={true}
+                label="勤務"
+                value={c.kinmu_index}
+                onChange={this.handleChangeC6KinmuIndex(c.index)}
+                fullWidth={true}
+              >
+                {this.props.kinmus.map(kinmu => (
+                  <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                label="連続日数上限"
+                type="number"
+                defaultValue={c.max_number_of_days}
+                onChange={this.handleChangeC6MaxNumberOfDays(c.index)}
+                fullWidth={true}
+              />
+            </ExpansionPanelDetails>
+            <ExpansionPanelActions>
+              <Button size="small" onClick={this.handleClickDeleteC6(c.index)}>削除</Button>
+            </ExpansionPanelActions>
+          </ExpansionPanel>
+        ))}
+        {this.props.kinmus.length === 0 ?
+          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+            <DialogTitle>勤務の連続日数の上限を追加できません</DialogTitle>
+            <DialogContent>
+              {this.props.kinmus.length === 0 ? <DialogContentText>勤務がありません</DialogContentText> : null}
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={this.handleCloseDialog}>閉じる</Button>
+            </DialogActions>
+          </Dialog> :
+          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+            <DialogTitle>勤務の連続日数の上限の追加</DialogTitle>
+            <DialogContent style={{ display: 'flex' }}>
+              <TextField
+                select={true}
+                label="勤務"
+                value={this.state.kinmu_index}
+                onChange={this.handleChangeNewC6KinmuIndex}
+                fullWidth={true}
+              >
+                {this.props.kinmus.map(kinmu => (
+                  <MenuItem key={kinmu.index} value={kinmu.index}>{kinmu.name}</MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                label="連続日数上限"
+                type="number"
+                defaultValue={this.state.max_number_of_days}
+                onChange={this.handleChangeNewC6MaxNumberOfDays}
+                fullWidth={true}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={this.handleClickCreateC6}>追加</Button>
+            </DialogActions>
+          </Dialog>}
+      </>
+    )
+  }
 }
 
 function mapStateToProps(state: all.State) {
--- static/src/modules/c6.ts
+++ static/src/modules/c6.ts
@@ -1,9 +1,16 @@
+const CREATE_C6 = 'CREATE_C6'
 const UPDATE_C6_KINMU_INDEX = 'UPDATE_C6_KINMU_INDEX'
 const UPDATE_C6_MAX_NUMBER_OF_DAYS = 'UPDATE_C6_MAX_NUMBER_OF_DAYS'
 const DELETE_C6 = 'DELETE_C6'
 
 export type C6 = {
   index: number
+  kinmu_index: number
+  max_number_of_days: number
+}
+
+type CreateC6 = {
+  type: typeof CREATE_C6
   kinmu_index: number
   max_number_of_days: number
 }
@@ -26,9 +33,18 @@ type DeleteC6 = {
 }
 
 type Action =
-  |UpdateC6KinmuIndex
+  | CreateC6
+  | UpdateC6KinmuIndex
   | UpdateC6MaxNumberOfDays
   | DeleteC6
+
+export function createC6(kinmu_index: number, max_number_of_days: number): CreateC6 {
+  return {
+    kinmu_index,
+    max_number_of_days,
+    type: CREATE_C6,
+  }
+}
 
 export function updateC6KinmuIndex(index: number, kinmu_index: number): UpdateC6KinmuIndex {
   return {
@@ -59,6 +75,12 @@ const initialState: State = []
 
 export function reducer(state: State = initialState, action: Action): State {
   switch (action.type) {
+    case CREATE_C6:
+      return state.concat({
+        index: Math.max(...state.map(c => c.index)) + 1,
+        kinmu_index: action.kinmu_index,
+        max_number_of_days: action.max_number_of_days,
+      })
     case UPDATE_C6_KINMU_INDEX:
       return state.map(c => {
         if (c.index !== action.index) {
