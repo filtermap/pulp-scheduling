6eaf2ad7931ede24cb9ba4c2ca80d00e681c91e9
filtermap <reducefiltermap@gmail.com>
2018-07-17 18:55:57 +0900

    implement sequence deletion

--- static/src/components/RenzokuKinshiKinmus.tsx
+++ static/src/components/RenzokuKinshiKinmus.tsx
@@ -1,30 +1,43 @@
+import Button from '@material-ui/core/Button'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import * as all from '../modules/all'
 import * as kinmus from '../modules/kinmus'
 import * as renzoku_kinshi_kinmus from '../modules/renzoku_kinshi_kinmus'
 
 type Props = {
+  dispatch: Dispatch
   renzoku_kinshi_kinmus: renzoku_kinshi_kinmus.RenzokuKinshiKinmu[]
   kinmus: kinmus.Kinmu[]
 }
 
 function RenzokuKinshiKinmus(props: Props) {
+  const sequence_ids = Array.from(new Set(props.renzoku_kinshi_kinmus.map(renzoku_kinshi_kinmu => renzoku_kinshi_kinmu.sequence_id)))
+  function handleClickDeleteSequence(sequence_id: number) {
+    return (_: React.MouseEvent<HTMLButtonElement>) => {
+      props.dispatch(renzoku_kinshi_kinmus.deleteSequence(sequence_id))
+    }
+  }
   return (
     <>
       <Toolbar>
         <Typography variant="subheading">連続禁止勤務並び</Typography>
       </Toolbar>
-      {props.renzoku_kinshi_kinmus.map(renzoku_kinshi_kinmu => (
-        <ExpansionPanel key={renzoku_kinshi_kinmu.index}>
+      {sequence_ids.map(sequence_id => (
+        <ExpansionPanel key={sequence_id}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{`${renzoku_kinshi_kinmu.sequence_id},${renzoku_kinshi_kinmu.sequence_number},${props.kinmus.find(kinmu => kinmu.index === renzoku_kinshi_kinmu.kinmu_index)!.name}`}</Typography>
+            <Typography>{props.renzoku_kinshi_kinmus.filter(renzoku_kinshi_kinmu => renzoku_kinshi_kinmu.sequence_id === sequence_id).sort((a, b) => a.sequence_number - b.sequence_number).map(renzoku_kinshi_kinmu => props.kinmus.find(kinmu => kinmu.index === renzoku_kinshi_kinmu.kinmu_index)!.name).join(', ')}</Typography>
           </ExpansionPanelSummary>
+          <ExpansionPanelActions>
+            <Button size="small" onClick={handleClickDeleteSequence(sequence_id)}>削除</Button>
+          </ExpansionPanelActions>
         </ExpansionPanel>
       ))}
     </>
--- static/src/modules/renzoku_kinshi_kinmus.ts
+++ static/src/modules/renzoku_kinshi_kinmus.ts
@@ -1,3 +1,5 @@
+const DELETE_SEQUENCE = 'DELETE_SEQUENCE'
+
 export type RenzokuKinshiKinmu = {
   index: number
   sequence_id: number
@@ -5,10 +7,29 @@ export type RenzokuKinshiKinmu = {
   kinmu_index: number
 }
 
+type DeleteSequence = {
+  type: typeof DELETE_SEQUENCE
+  sequence_id: number
+}
+
+type Action =
+  | DeleteSequence
+
+export function deleteSequence(sequence_id: number): DeleteSequence {
+  return {
+    sequence_id,
+    type: DELETE_SEQUENCE,
+  }
+}
+
 export type State = RenzokuKinshiKinmu[]
 
 const initialState: State = []
 
-export function reducer(state: State = initialState): State {
+export function reducer(state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case DELETE_SEQUENCE:
+      return state.filter(renzoku_kinshi_kinmu => renzoku_kinshi_kinmu.sequence_id !== action.sequence_id)
+  }
   return state
 }
