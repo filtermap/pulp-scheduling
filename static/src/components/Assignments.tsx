342fc9aa7863b4080c61d24ecbd786b3cde008d4
filtermap <reducefiltermap@gmail.com>
2018-07-23 19:02:50 +0900

    show indeterminate progress bar

--- static/src/components/Assignments.tsx
+++ static/src/components/Assignments.tsx
@@ -2,11 +2,13 @@ import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import LinearProgress from '@material-ui/core/LinearProgress'
 import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -25,6 +27,9 @@ import * as assignments from '../modules/assignments'
 import * as utils from '../utils'
 
 const styles = createStyles({
+  dialogTableWrapper: {
+    overflow: 'auto',
+  },
   leftHeaderCell: {
     background: 'white',
     left: 0,
@@ -49,6 +54,7 @@ type Props = {
 
 type State = {
   open: boolean
+  inProgress: boolean
   assignments: assignments.Assignment[]
 }
 
@@ -66,6 +72,7 @@ function sortDateNames(dateNames: string[]): string[] {
 class Assignments extends React.Component<Props, State> {
   public state: State = {
     assignments: [],
+    inProgress: false,
     open: false,
   }
   public handleClickDeleteRoster(roster_id: number) {
@@ -89,11 +96,15 @@ class Assignments extends React.Component<Props, State> {
     this.setState({ open: false })
   }
   public handleClickSolve = async () => {
+    this.setState({ inProgress: true })
     const newAssignments = (await utils.sendJSONRPCRequest('solve', [this.props.all])).result
     if (newAssignments.length === 0) {
       throw Error()
     }
-    this.setState({ assignments: newAssignments })
+    this.setState({
+      assignments: newAssignments,
+      inProgress: false,
+    })
   }
   public handleClickCreateRoster = () => {
     this.setState({ open: false })
@@ -152,50 +163,58 @@ class Assignments extends React.Component<Props, State> {
             </ExpansionPanel>
           )
         })}
-        {this.state.assignments.length === 0 ?
-          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+        {this.state.inProgress ?
+          <Dialog open={this.state.open} fullWidth={true} maxWidth="md">
             <DialogTitle>勤務表の追加</DialogTitle>
             <DialogContent>
-              <Button size="small" onClick={this.handleClickSolve}>自動作成</Button>
+              <DialogContentText>作成中...</DialogContentText>
+              <LinearProgress variant="query" />
             </DialogContent>
-            <DialogActions>
-              <Button color="primary" onClick={this.handleCloseDialog}>閉じる</Button>
-            </DialogActions>
           </Dialog> :
-          <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
-            <DialogTitle>勤務表の追加</DialogTitle>
-            <DialogContent style={{ display: 'flex' }}>
-              <div className={this.props.classes.tableWrapper}>
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell padding="dense" className={this.props.classes.topHeaderCell}>\</TableCell>
-                      {new_date_names.map(date_name => (
-                        <TableCell key={date_name} padding="dense" className={this.props.classes.topHeaderCell}>{date_name}</TableCell>
-                      ))}
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {members_by_new_assignments.map(member => {
-                      const assignments_by_member_index = this.state.assignments.filter(assignment => assignment.member_index === member.index)
-                      return (
-                        <TableRow key={member.index}>
-                          <TableCell padding="dense" className={this.props.classes.leftHeaderCell}>{member.name}</TableCell>
-                          {new_date_names.map(date_name => (
-                            <TableCell padding="dense" key={date_name}>{this.props.all.kinmus.find(kinmu => kinmu.index === assignments_by_member_index.find(assignment => assignment.date_name === date_name)!.kinmu_index)!.name}</TableCell>
-                          ))}
-                        </TableRow>
-                      )
-                    })}
-                  </TableBody>
-                </Table>
-              </div>
-            </DialogContent>
-            <DialogActions>
-              <Button color="primary" onClick={this.handleClickCreateRoster}>追加</Button>
-              <Button color="primary" onClick={this.handleCloseDialog}>閉じる</Button>
-            </DialogActions>
-          </Dialog>}
+          this.state.assignments.length === 0 ?
+            <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+              <DialogTitle>勤務表の追加</DialogTitle>
+              <DialogContent>
+                <Button size="small" onClick={this.handleClickSolve}>自動作成</Button>
+              </DialogContent>
+              <DialogActions>
+                <Button color="primary" onClick={this.handleCloseDialog}>閉じる</Button>
+              </DialogActions>
+            </Dialog> :
+            <Dialog onClose={this.handleCloseDialog} open={this.state.open} fullWidth={true} maxWidth="md">
+              <DialogTitle>勤務表の追加</DialogTitle>
+              <DialogContent style={{ display: 'flex' }}>
+                <div className={this.props.classes.dialogTableWrapper}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell padding="dense" className={this.props.classes.topHeaderCell}>\</TableCell>
+                        {new_date_names.map(date_name => (
+                          <TableCell key={date_name} padding="dense" className={this.props.classes.topHeaderCell}>{date_name}</TableCell>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {members_by_new_assignments.map(member => {
+                        const assignments_by_member_index = this.state.assignments.filter(assignment => assignment.member_index === member.index)
+                        return (
+                          <TableRow key={member.index}>
+                            <TableCell padding="dense" className={this.props.classes.leftHeaderCell}>{member.name}</TableCell>
+                            {new_date_names.map(date_name => (
+                              <TableCell padding="dense" key={date_name}>{this.props.all.kinmus.find(kinmu => kinmu.index === assignments_by_member_index.find(assignment => assignment.date_name === date_name)!.kinmu_index)!.name}</TableCell>
+                            ))}
+                          </TableRow>
+                        )
+                      })}
+                    </TableBody>
+                  </Table>
+                </div>
+              </DialogContent>
+              <DialogActions>
+                <Button color="primary" onClick={this.handleClickCreateRoster}>追加</Button>
+                <Button color="primary" onClick={this.handleCloseDialog}>閉じる</Button>
+              </DialogActions>
+            </Dialog>}
       </>
     )
   }
