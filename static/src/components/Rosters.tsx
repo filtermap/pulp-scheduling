ca08728c630a7afd6d03b0ccb6e265a5fc41d12a
filtermap <reducefiltermap@gmail.com>
2018-08-06 21:44:05 +0900

    roster cards

--- static/src/components/Rosters.tsx
+++ static/src/components/Rosters.tsx
@@ -4,10 +4,7 @@ import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import ExpansionPanel from '@material-ui/core/ExpansionPanel'
-import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import Grid from '@material-ui/core/Grid'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table'
@@ -17,8 +14,6 @@ import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import * as iconv from 'iconv-lite'
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
@@ -26,27 +21,7 @@ import { StateWithHistory } from 'redux-undo'
 import * as all from '../modules/all'
 import * as assignments from '../modules/assignments'
 import * as utils from '../utils'
-
-const styles = createStyles({
-  dialogTableWrapper: {
-    overflow: 'auto',
-  },
-  leftHeaderCell: {
-    background: 'white',
-    left: 0,
-    position: 'sticky',
-  },
-  tableWrapper: {
-    maxHeight: 'calc(100vh - 200px)',
-    overflow: 'auto',
-  },
-  topHeaderCell: {
-    background: 'white',
-    position: 'sticky',
-    top: 0,
-    zId: 1,
-  },
-})
+import Roster from './Roster'
 
 type Props = {
   dispatch: Dispatch
@@ -58,8 +33,6 @@ type State = {
   inProgress: boolean
   errorMessage: string
   newRosterAssignments: assignments.Assignment[]
-  deletionDialogIsOpen: boolean
-  selectedRosterId: number
 }
 
 const dateNamePattern = /^(\d{4})-(\d{1,2})-(\d{1,2})$/
@@ -76,11 +49,9 @@ function sortDateNames(dateNames: string[]): string[] {
 class Rosters extends React.Component<Props, State> {
   public state: State = {
     creationDialogIsOpen: false,
-    deletionDialogIsOpen: false,
     errorMessage: '',
     inProgress: false,
     newRosterAssignments: [],
-    selectedRosterId: this.props.all.assignments.length > 0 ? this.props.all.assignments[0].roster_id : 0,
   }
   public handleClickOpenCreationDialog = () => {
     this.setState({ creationDialogIsOpen: true })
@@ -107,84 +78,27 @@ class Rosters extends React.Component<Props, State> {
     this.setState({ creationDialogIsOpen: false })
     this.props.dispatch(all.createRoster(this.state.newRosterAssignments))
   }
-  public handleClickOpenDeletionDialog(selectedRosterId: number) {
-    return () => {
-      this.setState({
-        deletionDialogIsOpen: true,
-        selectedRosterId,
-      })
-    }
-  }
-  public handleCloseDeletionDialog = () => {
-    this.setState({ deletionDialogIsOpen: false })
-  }
-  public handleClickDeleteRoster = () => {
-    this.setState({ deletionDialogIsOpen: false })
-    this.props.dispatch(all.deleteRoster(this.state.selectedRosterId))
-  }
-  public handleClickExportToCSV(roster_id: number) {
-    return async () => {
-      const assignments_by_roster_id = this.props.all.assignments.filter(assignment => assignment.roster_id === roster_id)
-      const csv = iconv.encode((await utils.sendJSONRPCRequest('download_csv', [assignments_by_roster_id, this.props.all.members, this.props.all.kinmus])).result, 'Shift_JIS')
-      const a = document.createElement('a')
-      a.download = `勤務表${roster_id}.csv`
-      a.href = URL.createObjectURL(new Blob([csv], { type: 'text/csv' }))
-      a.click()
-    }
-  }
   public render() {
-    const date_names = sortDateNames(Array.from(new Set(this.props.all.assignments.map(assignment => assignment.date_name))))
-    const new_date_names = sortDateNames(Array.from(new Set(this.state.newRosterAssignments.map(assignment => assignment.date_name))))
-    const members_by_new_assignments = this.props.all.members.filter(member => this.state.newRosterAssignments.some(assignment => assignment.member_id === member.id))
-    const selectedRosterAssignments = this.props.all.assignments.filter(assignment => assignment.roster_id === this.state.selectedRosterId)
+    const newRosterDateNames = sortDateNames(Array.from(new Set(this.state.newRosterAssignments.map(({ date_name }) => date_name))))
+    const newRosterMemberIds = Array.from(new Set(this.state.newRosterAssignments.map(({ member_id }) => member_id)))
+    const newRosterMembers = this.props.all.members.filter(({ id }) => newRosterMemberIds.includes(id))
     return (
       <>
-        <Toolbar>
-          <Typography variant="subheading" style={{ flex: 1 }}>勤務表</Typography>
-          <Button size="small" onClick={this.handleClickOpenCreationDialog}>追加</Button>
-        </Toolbar>
-        {this.props.all.rosters.map(roster => {
-          const assignments_by_roster_id = this.props.all.assignments.filter(assignment => assignment.roster_id === roster.id)
-          const members_by_assignments = this.props.all.members.filter(member => assignments_by_roster_id.some(assignment => assignment.member_id === member.id))
-          return (
-            <ExpansionPanel key={roster.id}>
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography>{`勤務表${roster.id}`}</Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <div className={this.props.classes.tableWrapper}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell padding="dense" className={this.props.classes.topHeaderCell}>\</TableCell>
-                        {date_names.map(date_name => (
-                          <TableCell key={date_name} padding="dense" className={this.props.classes.topHeaderCell}>{date_name}</TableCell>
-                        ))}
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {members_by_assignments.map(member => {
-                        const assignments_by_roster_id_and_member_id = assignments_by_roster_id.filter(assignment => assignment.member_id === member.id)
-                        return (
-                          <TableRow key={member.id}>
-                            <TableCell padding="dense" className={this.props.classes.leftHeaderCell}>{member.name}</TableCell>
-                            {date_names.map(date_name => (
-                              <TableCell padding="dense" key={date_name}>{this.props.all.kinmus.find(kinmu => kinmu.id === assignments_by_roster_id_and_member_id.find(assignment => assignment.date_name === date_name)!.kinmu_id)!.name}</TableCell>
-                            ))}
-                          </TableRow>
-                        )
-                      })}
-                    </TableBody>
-                  </Table>
-                </div>
-              </ExpansionPanelDetails>
-              <ExpansionPanelActions>
-                <Button size="small" onClick={this.handleClickOpenDeletionDialog(roster.id)}>削除</Button>
-                <Button size="small" onClick={this.handleClickExportToCSV(roster.id)}>CSV出力</Button>
-              </ExpansionPanelActions>
-            </ExpansionPanel>
-          )
-        })}
+        <div style={{ padding: 8 }}>
+          <Grid container={true} spacing={8}>
+            <Grid item={true} xs={12}>
+              <Toolbar>
+                <Typography variant="subheading" style={{ flex: 1 }}>勤務表</Typography>
+                <Button size="small" onClick={this.handleClickOpenCreationDialog}>追加</Button>
+              </Toolbar>
+            </Grid>
+            {this.props.all.rosters.map(roster => (
+              <Grid key={roster.id} item={true} xs={12}>
+                <Roster roster={roster} />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
         {this.state.inProgress ?
           <Dialog open={this.state.creationDialogIsOpen} fullWidth={true} maxWidth="md">
             <DialogTitle>勤務表の追加</DialogTitle>
@@ -222,19 +136,19 @@ class Rosters extends React.Component<Props, State> {
                       <TableHead>
                         <TableRow>
                           <TableCell padding="dense" className={this.props.classes.topHeaderCell}>\</TableCell>
-                          {new_date_names.map(date_name => (
+                          {newRosterDateNames.map(date_name => (
                             <TableCell key={date_name} padding="dense" className={this.props.classes.topHeaderCell}>{date_name}</TableCell>
                           ))}
                         </TableRow>
                       </TableHead>
                       <TableBody>
-                        {members_by_new_assignments.map(member => {
-                          const assignments_by_member_id = this.state.newRosterAssignments.filter(assignment => assignment.member_id === member.id)
+                        {newRosterMembers.map(member => {
+                          const newRosterMemberAssignments = this.state.newRosterAssignments.filter(assignment => assignment.member_id === member.id)
                           return (
                             <TableRow key={member.id}>
                               <TableCell padding="dense" className={this.props.classes.leftHeaderCell}>{member.name}</TableCell>
-                              {new_date_names.map(date_name => (
-                                <TableCell padding="dense" key={date_name}>{this.props.all.kinmus.find(kinmu => kinmu.id === assignments_by_member_id.find(assignment => assignment.date_name === date_name)!.kinmu_id)!.name}</TableCell>
+                              {newRosterDateNames.map(date_name => (
+                                <TableCell padding="dense" key={date_name}>{this.props.all.kinmus.find(kinmu => kinmu.id === newRosterMemberAssignments.find(assignment => assignment.date_name === date_name)!.kinmu_id)!.name}</TableCell>
                               ))}
                             </TableRow>
                           )
@@ -248,18 +162,6 @@ class Rosters extends React.Component<Props, State> {
                   <Button color="primary" onClick={this.handleCloseCreationDialog}>閉じる</Button>
                 </DialogActions>
               </Dialog>}
-        {selectedRosterAssignments.length > 0 &&
-          <Dialog onClose={this.handleCloseDeletionDialog} open={this.state.deletionDialogIsOpen} fullWidth={true} maxWidth="md">
-            <DialogTitle>勤務表の削除</DialogTitle>
-            <DialogContent>
-              <DialogContentText>この勤務表を削除します</DialogContentText>
-              <Typography>{`勤務表${this.state.selectedRosterId}`}</Typography>
-            </DialogContent>
-            <DialogActions>
-              <Button color="primary" onClick={this.handleClickDeleteRoster}>削除</Button>
-              <Button color="primary" onClick={this.handleCloseDeletionDialog}>閉じる</Button>
-            </DialogActions>
-          </Dialog>}
       </>
     )
   }
@@ -271,4 +173,25 @@ function mapStateToProps(state: StateWithHistory<all.State>) {
   }
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(Rosters))
+const styles = createStyles({
+  dialogTableWrapper: {
+    overflow: 'auto',
+  },
+  leftHeaderCell: {
+    background: 'white',
+    left: 0,
+    position: 'sticky',
+  },
+  tableWrapper: {
+    maxHeight: 'calc(100vh - 200px)',
+    overflow: 'auto',
+  },
+  topHeaderCell: {
+    background: 'white',
+    position: 'sticky',
+    top: 0,
+    zId: 1,
+  },
+})
+
+export default withStyles(styles)(connect(mapStateToProps)(Rosters))
