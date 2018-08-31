390d79d01bf6005cb660a895594b0275e924e805
filtermap <reducefiltermap@gmail.com>
2018-08-31 20:19:26 +0900

    use shell on build

--- build.py
+++ build.py
@@ -5,10 +5,10 @@ import subprocess
 project_root_directory = os.path.dirname(os.path.abspath(__file__))
 
 os.chdir(os.path.join(project_root_directory, "static"))
-subprocess.run(["npm", "run", "build"])
+subprocess.run("npm run build", shell=True)
 
 os.chdir(project_root_directory)
 if os.path.exists("dist"):
     shutil.rmtree("dist")
-subprocess.run(["pyinstaller", "main.spec", "--clean"])
+subprocess.run("pyinstaller main.spec --clean", shell=True)
 shutil.copytree("data", os.path.join("dist", "data"))
