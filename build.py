05290e6625a2f4212106f1c36c2abb646babadff
filtermap <reducefiltermap@gmail.com>
2021-04-29 18:28:36 +0900

    change copy function to copy

--- build.py
+++ build.py
@@ -11,5 +11,5 @@ os.chdir(project_root_directory)
 if os.path.exists("dist"):
     shutil.rmtree("dist")
 subprocess.run("pyinstaller main.spec --clean", shell=True)
-shutil.copytree("data", os.path.join("dist", "data"))
-shutil.copy2("README.md", "dist")
+shutil.copytree("data", os.path.join("dist", "data"), copy_function=copy)
+shutil.copy("README.md", "dist")
