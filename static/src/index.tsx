82a80234ce3cf8f7fc8161f4d9b51b817c81c485
filtermap <reducefiltermap@gmail.com>
2021-04-29 09:16:52 +0900

    fix import order

--- static/src/index.tsx
+++ static/src/index.tsx
@@ -1,8 +1,8 @@
 import blue from "@material-ui/core/colors/blue";
 import teal from "@material-ui/core/colors/teal";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import { MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
